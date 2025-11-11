da8a1683c51482772c4837fc790595bcca4f077f
Maria Eduarda <marizari2006@gmail.com>
2025-11-11 15:41:25 -0300

    Treinando Rotas - any e match

--- app/routes/web.php
+++ app/routes/web.php
@@ -6,6 +6,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//o que faz o rota any é aceitar qualquer tipo de requisição. No caso abaixo, aceita put, delete, get, post. Não é muito usado. por questões de segurança
+Route::any('/any', function(){
+    return "Pemite todo tipo de acesso http(put, delete, get, post))";
+
+});
+
+//o que faz o rota match é aceitar apenas os tipos de requisição que forem definidos. No caso abaixo, aceita apenas get e post
+Route::match(['get', 'post'], '/match', function(){
+    return "Permite apenas get e post,
+    ou seja, permite SOMENTE métodos definidos como foi no caso do verbo http get e post";
+// permite apenas os métodos definidos
+});   
+
+
 Route::get('/empresa', function(){
     return view('site.empresa');
 
