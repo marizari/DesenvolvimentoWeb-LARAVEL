8b26cc67ccf2a23de5522dbeff4f35c71aca921a
Maria Eduarda <marizari2006@gmail.com>
2025-11-20 14:31:33 -0300

    update rotas e partindo pro controllers

---
+++ app/app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request; //diretório que armazena todas as entidades do LARAVEL, servisos prontos usarmos que estão nas pasta VENDOR
+
+class ProdutoController extends Controller
+{
+    public function index() {
+        return "index";
+    }
+}
